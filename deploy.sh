5215ae74f4c54cd7e6f958d684a2f3102cacbee6
gabrielmontplaisir <gabriel.montplaisir@protonmail.com>
2022-12-26 22:01:46 -0500

    Traded Aria-label for .sr-only class

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GabrielMontplaisir/frontend-mentor.git main
+git push -f git@github.com:GabrielMontplaisir/fem-product-card.git main
 
 cd -
